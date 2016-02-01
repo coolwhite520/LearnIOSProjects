f6ba24b28bbf0709a4027421dade196123677448
baiyang <81929154@qq.com>
2016-02-01 13:39:04 +0800

    AFNetworking

---
+++ 网络相关操作/MyAFNetWorking/MyAFNetWorking/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  MyAFNetWorking
+//
+//  Created by 白洋 on 16/2/1.
+//  Copyright © 2016年 白洋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 网络相关操作/MyAFNetWorking/MyAFNetWorking/ViewController.m
@@ -0,0 +1,67 @@
+//
+//  ViewController.m
+//  MyAFNetWorking
+//
+//  Created by 白洋 on 16/2/1.
+//  Copyright © 2016年 白洋. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "AFURLSessionManager.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (IBAction)btnUpload:(id)sender {
+    
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
+    
+    NSURL *URL = [NSURL URLWithString:@"http://example.com/upload"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:URL];
+    
+    NSURL *filePath = [NSURL fileURLWithPath:@"file://path/to/image.png"];
+    NSURLSessionUploadTask *uploadTask = [manager uploadTaskWithRequest:request fromFile:filePath progress:nil completionHandler:^(NSURLResponse *response, id responseObject, NSError *error) {
+        if (error) {
+            NSLog(@"Error: %@", error);
+        } else {
+            NSLog(@"Success: %@ %@", response, responseObject);
+        }
+    }];
+    [uploadTask resume];
+    
+}
+
+- (IBAction)btnGet:(UIButton *)sender {
+    
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
+    
+    NSURL *URL = [NSURL URLWithString:@"https://codeload.github.com/pokeb/asi-http-request/zip/master"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:URL];
+    
+    NSURLSessionDownloadTask *downloadTask = [manager downloadTaskWithRequest:request progress:nil destination:^NSURL *(NSURL *targetPath, NSURLResponse *response) {
+        
+        NSURL *documentsDirectoryURL = [[NSFileManager defaultManager] URLForDirectory:NSDocumentDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:NO error:nil];
+        return [documentsDirectoryURL URLByAppendingPathComponent:[response suggestedFilename]];
+        
+    } completionHandler:^(NSURLResponse *response, NSURL *filePath, NSError *error) {
+        NSLog(@"File downloaded to: %@", filePath);
+    }];
+    [downloadTask resume];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
